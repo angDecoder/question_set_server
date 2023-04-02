4af055e268487802b12431b38d4609141dbcaee0
angDecoder <angshudas012@gmail.com>
2023-04-03 00:35:54 +0530

    register user route added

--- controller/userController.js
+++ controller/userController.js
@@ -1,15 +1,56 @@
 const pool = require('../dbconfig');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const registerUser = async (req,res)=>{
+    const { email,password,username } = req.body;
 
+    if( !email || !password || !username ){
+        res.status(401).json({ message : 'email, username and password are required' });
+        return;
+    }
+
+    try {
+        const hashedPassword = await bcrypt.hash(password,10);
+        await pool.query(`
+            INSERT INTO USERS(USERNAME,EMAIL,PASSWORD)
+            VALUES ($1,$2,$3);
+        `,[username,email,hashedPassword]);
+
+        res.status(201).json({ message : 'new user created' });
+    } catch (error) {
+        // console.log(error);
+        res.status(400).json({ message : 'user not created',error });
+    }
 }
 
 const loginUser = async (req,res)=>{
+    const { email,password } = req.body;
+    console.log('here');
+    if( !email || !password ){
+        res.status(401).json({ message : 'email and password are required' });
+        return;
+    }
 
+    try {
+        const result = await pool.query(`
+            SELECT PASSWORD FROM USERS
+            WHERE EMAIL = $1
+        `,[email]);
+        const hash = result?.rows[0]?.password;
+        const compare = await bcrypt.compare(password,hash);
+
+        if( compare )
+            res.json({ message : 'user logged in' });
+        else 
+            res.status(401).json({ message : 'wrong password, try again' });
+    } catch (error) {
+        res.status(400).json({ message : 'some error occured',error });
+    }
 }
 
 const autoLogin = async (req,res)=>{
-
+    
 }
 
 const refresh = async (req,res)=>{
--- server.js
+++ server.js
@@ -1,16 +1,30 @@
 const express = require('express');
 const cors = require('cors');
+const pool = require('./dbconfig');
 require('dotenv').config();
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/check',(req,res)=>{
     res.json({ msg : "done" });
 });
 
-app.route('/user',require('./routes/User'));
+app.use('/user',require('./routes/User'));
 
+const startServer = async()=>{
 
-app.listen(3000,()=>{
-    console.log('listening to port 3000');
-})
+    try {
+        const res = await pool.query(`select 'connected to db' as sum`);
+        app.listen(3500,async()=>{
+            console.log(res.rows[0].sum);
+            console.log('listening to port 3500');
+        })
+    } catch (error) {
+        console.log('some error occured');
+    }
+}
+
+startServer();
